9081fe16c2b0f9100fdfd371601339e759f12133
hyrorre <hiromu.yamashita@hotmail.com>
2024-09-14 08:44:25 +0900

    Refactor: run prettier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,17 +2,16 @@
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+  <title>Home</title>
+  <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <section>
-	<h1>
-		Welcome to your new<br />SvelteKit app
-	</h1>
+  <h1>
+    Welcome to your new<br />SvelteKit app
+  </h1>
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
+  <h2>
+    try editing <strong>src/routes/+page.svelte</strong>
+  </h2>
 </section>
