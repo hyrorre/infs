37b89032166734905af093f9d0dc1047b6e6e8e0
hyrorre <hiromu.yamashita@hotmail.com>
2024-09-29 21:46:36 +0900

    Fix: supabase redirect options

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,11 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/app.css'],
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@nuxtjs/supabase']
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/eslint', '@nuxtjs/supabase'],
+  supabase: {
+    redirectOptions: {
+      login: '/',
+      callback: '/'
+    }
+  }
 })
