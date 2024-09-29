5a3636df826585cc6b4de76bfaa006ed8e9d55b9
hyrorre <hiromu.yamashita@hotmail.com>
2024-09-29 16:11:29 +0900

    Fix: eslint config (vue/html-self-closing)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,3 +2,16 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt()
+  .prepend()
+  .override('', {
+    rules: {
+      'vue/html-self-closing': [
+        'error',
+        {
+          html: {
+            void: 'always'
+          }
+        }
+      ]
+    }
+  })
