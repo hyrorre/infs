b8fdc483560739b9085917112da0b62546d2a9b4
hyrorre <hiromu.yamashita@hotmail.com>
2025-05-04 22:06:32 +0900

    Fix: dummy api

--- server/api/dummy.post.ts
+++ server/api/dummy.post.ts
@@ -1,12 +1,16 @@
-import { serverSupabaseClient } from '#supabase/server'
+import { serverSupabaseServiceRole } from '#supabase/server'
 
 export default defineEventHandler(async (event) => {
-  const client = await serverSupabaseClient(event)
+  const client = await serverSupabaseServiceRole(event)
 
-  await client.from('dummys').upsert({
+  const { data, error } = await client.from('dummys').upsert({
     id: 1,
-    update_at: new Date()
+    updated_at: new Date()
   } as any)
 
+  if (error) {
+    console.error('Error updating dummy:', error)
+    throw createError({ statusCode: 500, message: 'Internal Server Error' })
+  }
   return 'The dummy has been updated.'
 })
